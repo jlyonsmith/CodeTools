fc741872086e6c0a4c924bd4d339c44c78b29a5b
John Lyon-Smith <john@lyon-smith.org>
2017-02-11 09:45:45 -0800

    Fix bug with converting tab size to integer

--- test/spacer/test-spacer.sh
+++ test/spacer/test-spacer.sh
@@ -11,3 +11,7 @@ eval $SPACER -m spaces -o Test.spaces.cs Test.cs
 eval $SPACER -m tabs -o Test.tabs.tson Test.cs
 eval $SPACER -m tabs -o Test.spaces.tson Test.tson
 eval $SPACER -m spaces -o Test.tabs.tson Test.tson
+eval $SPACER -m tabs -t 8 -o Test.spaces.tson Test.tson
+eval $SPACER -m spaces -t 4 -o Test.tabs.tson Test.tson
+eval $SPACER -m tabs -r -o Test.spaces.tson Test.tson
+eval $SPACER -m spaces -r -o Test.tabs.tson Test.tson
